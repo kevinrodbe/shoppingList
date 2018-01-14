f3351106cb90beb0743fe98ed3d34c4e7b433c7f
kevinrodbe <kevin.rodriguez992@gmail.com>
2018-01-13 22:43:57 -0500

    feat(s2-4): add firebase connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
+import { FIREBASE_CONFIG } from './firebase.credentials';
 import { MyApp } from './app.component';
 
 @NgModule({
   declarations: [MyApp],
-  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireDatabaseModule
+  ],
   bootstrap: [IonicApp],
   entryComponents: [MyApp],
   providers: [StatusBar, SplashScreen, { provide: ErrorHandler, useClass: IonicErrorHandler }]
