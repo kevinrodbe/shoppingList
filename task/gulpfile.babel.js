7e26ec73da198b61cb7aa86688bf81d3db7a5934
kevinrodbemx <kevin@multiplica.com>
2018-01-17 19:33:03 -0500

    WIP

--- task/gulpfile.babel.js
+++ task/gulpfile.babel.js
@@ -3,46 +3,53 @@ import request from 'request'
 import rp from 'request-promise'
 // const fs = Promise.promisifyAll(require('fs'))
 import fs from 'fs'
+import mustache from 'mustache'
 
 const uri = 'https://oim.mapfre.com.pe/oim_webproc/api/descriptor'
+const tplServices = './task/template.service.ts'
+let asd
 
-var download = function(url, dest, cb) {
-  var file = fs.createWriteStream(dest);
-  //var req2 = request(url).pipe(file)
-  var req2 = rp(url)
-    .then(resp => resp)
-    .then()
+function download(url) {
+  // var file = fs.createWriteStream(dest);
+  // var req2 = request(url).pipe(file)
+  rp(url)
+    .then((jsonDescriptor) => {
+      asd = jsonDescriptor;
+      return readFile(tplServices)
+    })
+    .then(generateFileFromTpl)
+    .then(writeServiceTs)
 };
 
-download(uri, 'asd.txt')
+download(uri)
 
 // download
 // read
 // render tpl
 // write
 
-function readFile(file, options) {
+function generateFileFromTpl(tpl) {
+  return new Promise((resolve) => {
+    resolve(mustache.render(tpl.data, asd))
+  })
+}
+
+function readFile(file) {
   return new Promise((resolve, reject) => {
-    fs.readFile(file, (err, data) => {
-      if (err) {
-        reject({err})
-      } else {
-        resolve({data})
-      }
+    fs.readFile(file, 'utf8', (err, data) => {
+      err ? reject(new Error({err})) : resolve({data})
     })
   })
 }
 
-function writeFile(fileName, data, options) {
+function writeFile(fileName, data) {
   return new Promise((resolve, reject) => {
     fs.writeFile(fileName, data, 'utf8', err => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(true)
-      }
+      err ? reject(new Error(err)) : resolve(true)
     })
   })
 }
 
-writeFile('message.txt', 'Hello Node.js').then(function() {console.log('mx')})
+function writeServiceTs(data) {
+  writeFile('myService.ts', data)
+}
